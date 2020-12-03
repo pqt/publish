98f34e47efe48449698e5905a4208c8f491c9b5e
Austin Paquette <austin.paquette@forcir.com>
2020-12-03 13:27:05 -0800

    time to start testing the action

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: '@pqt',
+  env: {
+    node: true,
+    jest: true,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export async function run(): Promise<void> {}
+import { setFailed, startGroup, endGroup } from '@actions/core';
+
+/**
+ * The entrypoint for the GitHub Action
+ */
+export async function run(): Promise<void> {
+  try {
+    startGroup('Check Version');
+    endGroup();
+    startGroup('Publish to NPM');
+    endGroup();
+    startGroup('Publish to GPR');
+    endGroup();
+  } catch (error) {
+    console.log('Error');
+    setFailed(error);
+  }
+}
+
+run();
