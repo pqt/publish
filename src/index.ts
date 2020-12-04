8722e08c59bed4ad2070af15f8d2a98847dd7d34
Austin Paquette <austin.paquette@forcir.com>
2020-12-03 19:12:40 -0800

    add full context log

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import { setFailed, startGroup, endGroup } from '@actions/core';
+import { debug as log, setFailed, startGroup, endGroup } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
 
 /**
  * The entrypoint for the GitHub Action
  */
 export async function run(): Promise<void> {
   try {
+    startGroup('Complete context log');
+    console.log(JSON.stringify(context));
+    endGroup();
     startGroup('Check Version');
     endGroup();
     startGroup('Publish to NPM');
